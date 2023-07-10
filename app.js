7a8f46d1f1881484558f6f83827911f9ae563316
vpc-23 <virtualassistvpc@gmail.com>
2023-07-10 09:54:13 +0300

    animation

--- app.js
+++ app.js
@@ -11,5 +11,14 @@ burger.addEventListener("click", () => {
   !menu.classList.contains("on")
     ? menu.classList.add("on")
     : menu.classList.remove("on");
-  
+
 });
+
+//form reload
+window.onload = function () {
+  // Get the form element
+  var form = document.getElementById('form');
+
+  // Reset the form on page load
+  form.reset();
+}
